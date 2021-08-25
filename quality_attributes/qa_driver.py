abb3d8a7e90aa5cfe3997b55f69cc87011edeaed
m-zakeri <m-zakeri@live.com>
2021-08-25 23:35:18 +0430

    Update qa_driver.py

--- quality_attributes/qa_driver.py
+++ quality_attributes/qa_driver.py
@@ -2,3 +2,23 @@
 Driver module for quality attributes classes
 
 """
+
+import os
+
+import pandas as pd
+
+
+def create_benchmark():
+    root_dir_path = r'../benchmark/SF110/dataset2/'
+    project_files = [f for f in os.listdir(root_dir_path) if os.path.isfile(os.path.join(root_dir_path, f))]
+    print(project_files)
+    df_modularity = pd.read_csv(r'../benchmark/QualCode160/data_project_modularity/ALL2',)
+
+    for file_ in project_files:
+        pass
+
+
+
+
+if __name__ == '__main__':
+    create_benchmark()
